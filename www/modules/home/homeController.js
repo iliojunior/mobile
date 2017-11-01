5a20072b6bf6904a7edc8263d6a969cb88d13541
Raphael Vizoni <ravizoni@gmail.com>
2017-11-01 10:08:57 -0200

    #11152 - Problema na array de fotos da categoria

--- www/modules/home/homeController.js
+++ www/modules/home/homeController.js
@@ -11,27 +11,17 @@ angular.module("home", [])
 	anunciosFactory.getAllSetores().then( function (response) {
 		$scope.listaDeSetores = response;
 		$scope.isLoadingPhoto = true;
-		var arrayDeFotos = [];
 		for (var i = 0; i < $scope.listaDeSetores.length; i++) {
-			/*anunciosFactory.getFotoDaCategoriaPorId($scope.listaDeSetores[i].idFoto)
-				.then(function (respostaDaFoto) {
-					$scope.listaDeSetores[0].imagem = respostaDaFoto;
-					$scope.isLoadingPhoto = false;
-				})*/
-			//===============================
-			$scope.listaDeSetores[i].imagem = null;
+		  	var itemLista = $scope.listaDeSetores[i]
+			itemLista.imagem = null;
 			getFotoDaCategoria($scope.listaDeSetores[i].idFoto)
 				.then( function (response) {
-					console.log("resposta: ",response);
-					//$scope.listaDeSetores[i].imagem = response;
-					arrayDeFotos.push(response);
+					console.log($scope.listaDeSetores)
+					itemLista.imagem = response;					
+					// console.log($scope.listaDeSetores)
 				})
 		}
 
-		for (var i = 0; i > $scope.listaDeSetores.length; i++) {
-			$scope.listaDeSetores[i].imagem = arrayDeFotos[i];
-			console.log($scope.listaDeSetores[i].imagem)
-		}
 
 	})
 
